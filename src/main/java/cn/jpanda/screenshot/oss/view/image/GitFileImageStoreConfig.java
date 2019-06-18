b4c5060b3feb4181355ec913f4ebcb1c4178d445
hanqi <jpanda@aliyun.com>
2019-06-18 17:56:22 +0800

    Code refactoring -- add git

--- src/main/java/cn/jpanda/screenshot/oss/view/image/GitFileImageStoreConfig.java
+++ src/main/java/cn/jpanda/screenshot/oss/view/image/GitFileImageStoreConfig.java
@@ -3,6 +3,9 @@ package cn.jpanda.screenshot.oss.view.image;
 import cn.jpanda.screenshot.oss.core.Configuration;
 import cn.jpanda.screenshot.oss.core.annotations.Controller;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,6 +13,13 @@ import java.util.ResourceBundle;
 @Controller
 public class GitFileImageStoreConfig implements Initializable {
 
+    public TextField localRepositoryDir;
+    public TextField subDir;
+    public TextField remoteRepositoryUrl;
+    public TextField branch;
+    public TextField username;
+    public CheckBox async;
+    public PasswordField password;
     private Configuration configuration;
 
     public GitFileImageStoreConfig(Configuration configuration) {
