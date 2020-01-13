8f6efb9cb27c0083f0a6501246f4c3f6bf9dc768
jpanda <jpanda@aliyun.com>
2020-01-13 10:51:41 +0800

    ##  添加动画

--- src/main/java/cn/jpanda/screenshot/oss/common/toolkit/PopDialog.java
+++ src/main/java/cn/jpanda/screenshot/oss/common/toolkit/PopDialog.java
@@ -1,14 +1,22 @@
 package cn.jpanda.screenshot.oss.common.toolkit;
 
+import javafx.animation.*;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.stage.StageStyle;
 import javafx.stage.Window;
+import javafx.util.Duration;
+
+import java.util.Optional;
 
 /**
  * 弹窗
@@ -26,6 +34,8 @@ public class PopDialog extends Dialog<ButtonType> {
     private SimpleObjectProperty<Parent> header = new SimpleObjectProperty<>(new HBox());
 
     private SimpleObjectProperty<Parent> content = new SimpleObjectProperty<>(new AnchorPane());
+
+    private SimpleObjectProperty<Animation> animation = new SimpleObjectProperty<>();
 
     private SimpleObjectProperty<Callable<Boolean, ButtonType>> callableSimpleObjectProperty = new SimpleObjectProperty<>(null);
 
@@ -51,14 +61,55 @@ public class PopDialog extends Dialog<ButtonType> {
         initStyle(StageStyle.TRANSPARENT);
         loadStylesheets();
         initButtonTypes();
+        initAnimation();
+        getDialogPane().parentProperty().addListener(new ChangeListener<Parent>() {
+            @Override
+            public void changed(ObservableValue<? extends Parent> observable, Parent oldValue, Parent newValue) {
+                System.out.println(321);
+            }
+        });
+        getDialogPane().sceneProperty().addListener(new ChangeListener<Scene>() {
+            @Override
+            public void changed(ObservableValue<? extends Scene> observable, Scene oldValue, Scene newValue) {
+                System.out.println("123");
+                if (Optional.ofNullable(newValue).isPresent()) {
+                    newValue.setFill(Color.TRANSPARENT);
+                }
+            }
+        });
     }
 
+    public void initAnimation() {
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.ZERO,
+                        new KeyValue(getDialogPane().scaleXProperty(), 0, Interpolator.EASE_BOTH),
+                        new KeyValue(getDialogPane().scaleYProperty(), 0, Interpolator.EASE_BOTH),
+                        new KeyValue(getDialogPane().visibleProperty(), false, Interpolator.EASE_BOTH)
+                ),
+                new KeyFrame(Duration.millis(10),
+                        new KeyValue(getDialogPane().visibleProperty(), true, Interpolator.EASE_BOTH),
+                        new KeyValue(getDialogPane().opacityProperty(), 0, Interpolator.EASE_BOTH)
+                ),
+                new KeyFrame(Duration.millis(300),
+                        new KeyValue(getDialogPane().scaleXProperty(), 1, Interpolator.EASE_BOTH),
+                        new KeyValue(getDialogPane().scaleYProperty(), 1, Interpolator.EASE_BOTH),
+                        new KeyValue(getDialogPane().opacityProperty(), 1, Interpolator.EASE_BOTH)
+                ));
+
+        timeline.setDelay(Duration.seconds(0));
+        this.animation.set(timeline);
+        showingProperty().addListener((observable, oldValue, newValue) -> {
+            getDialogPane().getScene().setFill(Color.TRANSPARENT);
+            timeline.play();
+        });
+
+    }
 
     protected void initDialogPane() {
         DialogPane dialogPane = new DialogPane() {
             @Override
             protected Node createButton(ButtonType buttonType) {
-                if (null==callableSimpleObjectProperty.get()){
+                if (null == callableSimpleObjectProperty.get()) {
                     return super.createButton(buttonType);
                 }
 
@@ -69,11 +120,21 @@ public class PopDialog extends Dialog<ButtonType> {
                 button.setCancelButton(buttonData.isCancelButton());
                 button.addEventHandler(ActionEvent.ACTION, ae -> {
                     if (callableSimpleObjectProperty.get().apply(buttonType)) {
+                        if (Optional.ofNullable(animation.get()).isPresent()) {
+                            animation.get().setAutoReverse(true);
+                            animation.get().setRate(-1);
+                            animation.get().play();
+                            animation.get().setOnFinished(e -> {
+                                close();
+                            });
+                        }
+                    } else {
                         close();
                     }
                 });
                 return button;
             }
+
         };
         setDialogPane(dialogPane);
     }
